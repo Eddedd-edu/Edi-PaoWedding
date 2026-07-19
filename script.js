2b18a418831f28b21919504c8ee3ea0ba4d8b772
Eddedd-edu <Edison.avendano@sedtolima.edu.co>
2026-07-19 11:40:32 -0500

    Fix formatting in script.js for consistency

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ window.addEventListener("load", function() {
         "ERES":   { nombre: "Padre y Madre", cupos: 2 },
         "NATA":   { nombre: "NATA", cupos: 1 },
         "ROBFAR":   { nombre: "Don Rowel y Tía Farly", cupos: 2 },
-        "CARKEV":   { nombre: "Tía Carmenza y Tío Kevin", cupos: 2 }
+        "CARKEV":   { nombre: "Tía Carmenza y Tío Kevin", cupos: 2 },
         "CAMI":   { nombre: "CAMILILLA", cupos: 1 },
         "KELLY":   { nombre: "KELLY JOHANA", cupos: 1 },
         "ROCI":   { nombre: "ROCI", cupos: 1 },
-        "VIVI":   { nombre: "VIVI", cupos: 1 },
+        "VIVI":   { nombre: "VIVI", cupos: 1 }
     };
 
     /* ====================================================
